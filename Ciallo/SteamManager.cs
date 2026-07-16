7eff5e8d71487a85175dbfce1a5f6377aa87e631
ShenCiao <3294833183@qq.com>
2026-07-16 21:12:54 +0800

    Use FacePunch steamworks api

---
+++ Ciallo/SteamManager.cs
@@ -0,0 +1,32 @@
+using Godot;
+using Steamworks;
+using System;
+
+namespace Ciallo;
+
+public partial class SteamManager : Node
+{
+    public const uint AppId = 4103990;
+
+    public override void _EnterTree()
+    {
+        try
+        {
+            SteamClient.Init(AppId, true);
+            if (SteamClient.IsValid)
+                GD.Print("Steam initialized successfully.");
+            else
+                throw new Exception();
+        }
+        catch (Exception e)
+        {
+            GD.PrintErr($"Steam initialization failed: {e.Message}");
+            return;
+        }
+    }
+
+    public override void _ExitTree()
+    {
+        SteamClient.Shutdown();
+    }
+}
